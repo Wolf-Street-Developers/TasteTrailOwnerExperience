4dd871147811fc881ff4c275ea008a171099c269
makedoniaz <jafar.sadig04@gmail.com>
2024-09-08 14:22:49 +0400

    Test pipeline

---
+++ TasteTrailOwnerExperience/src/TasteTrailOwnerExperience.Api/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TasteTrailOwnerExperience.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetNumber() {
+        return Ok(1);
+    }
+}
